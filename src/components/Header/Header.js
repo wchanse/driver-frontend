57780a08a6dfe493d9e396bc2c6452d247fcf318
Wayne Chan <wchan.it@outlook.com>
2021-08-06 10:13:36 -0500

    Removed references to fontawesome

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
